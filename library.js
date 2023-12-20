7a0642c125e69a5de6566ab31900898c6352b3cd
Austin Hogan <austinhogandev@gmail.com>
2023-12-20 15:54:52 -0500

    Set up project

---
+++ library.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function getLibrary() {
+    console.log(myLibrary);
+}
